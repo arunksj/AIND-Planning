e65d63157670016b74cba95fa0371e05dd19c04e
arunkumar.jadhav@gmail.com <arunkumar.jadhav@ITIN000002-MAC.Bluewireless.asia>
2017-05-24 14:09:12 +0530

    Got ignore preconditions working

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -48,7 +48,7 @@ class AirCargoProblem(Problem):
             list of Action objects
         """
 
-        # TODO create concrete Action objects based on the domain action schema for: Load, Unload, and Fly
+        # create concrete Action objects based on the domain action schema for: Load, Unload, and Fly
         # concrete actions definition: specific literal action that does not include variables as with the schema
         # for example, the action schema 'Load(c, p, a)' can represent the concrete actions 'Load(C1, P1, SFO)'
         # or 'Load(C2, P2, JFK)'.  The actions for the planning problem must be concrete because the problems in
@@ -207,9 +207,8 @@ class AirCargoProblem(Problem):
         executed.
         """
         # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
-        count = 0
-        return count
 
+        return len(self.goal)
 
 def air_cargo_p1() -> AirCargoProblem:
     cargos = ['C1', 'C2']
